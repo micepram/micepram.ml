e43a236a929b27d642706306e8a4a848695b07c6
Pramika Garg <micepram@gmail.com>
2025-05-28 03:19:51 +0530

    feat: add supernova awards

--- js/main.js
+++ js/main.js
@@ -125,7 +125,7 @@ function initScrollAnimations() {
     }, observerOptions);
 
     const animatedElements = document.querySelectorAll(
-        '.section-label, .about-intro, .about-secondary, .stat-orb, .timeline-item, .research-intro, .publication-card'
+        '.section-label, .about-intro, .about-secondary, .stat-orb, .timeline-item, .research-intro, .publication-card, .award-card'
     );
     
     animatedElements.forEach(el => observer.observe(el));
